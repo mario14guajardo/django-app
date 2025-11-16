687a7c59d3e0c82e92c282d4914337094b399bc6
mario14guajardo <mario14guajardo@gmail.com>
2025-11-15 21:16:46 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-gunicron myapp.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn myapp.wsgi:application --bind 0.0.0.0:$PORT
