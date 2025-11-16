a00b7ce26b13cb21408df497ae3569ccd3671e06
mario14guajardo <mario14guajardo@gmail.com>
2025-11-15 19:49:13 -0600

    Update settings.py

--- myapp/settings.py
+++ myapp/settings.py
@@ -1,6 +1,10 @@
 import os
 from pathlib import Path
 import dj_database_url
+from django.contrib.auth import REDIRECT_FIELD_NAME
+
+print("LOGIN_URL:", LOGIN_URL)
+print("REDIRECT_FIELD_NAME:", REDIRECT_FIELD_NAME)
 
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
