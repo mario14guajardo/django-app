c6195df1b5ad3bfc66cb47593634b718d59e50aa
mario14guajardo <mario14guajardo@gmail.com>
2025-11-15 20:48:41 -0600

    Update entrypoint.sh

--- myapp/entrypoint.sh
+++ myapp/entrypoint.sh
@@ -1,11 +1,4 @@
-#!/bin/bash
-set -e
-
-echo "Running migrations..."
+#!/bin/sh
 python manage.py migrate --noinput
-
-echo "Collecting static files..."
 python manage.py collectstatic --noinput
-
-echo "Starting Gunicorn..."
-gunicorn myapp.wsgi:application --bind 0.0.0.0:8000
+gunicorn myapp.wsgi:application --bind 0.0.0.0:$PORT
