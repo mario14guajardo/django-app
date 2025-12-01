09e71f65046a441c996772a7f75f78862db1ded5
mario14guajardo <mario14guajardo@gmail.com>
2025-12-01 13:46:20 -0600

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # ---------------------- PROFILE ----------------------
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.CharField(max_length=255, blank=True, null=True)
+    avatar_url = models.URLField(blank=True, null=True)
     bio = models.TextField(blank=True)
     # Add more fields for UI settings, badges, reputation, etc.
 
