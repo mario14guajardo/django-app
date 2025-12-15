5d5fd6ef2375fd0219a3955bc945de72348b694e
mario14guajardo <mario14guajardo@gmail.com>
2025-12-15 09:51:16 -0600

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
-from .models import Community, Post, Event, Badge, Profile, Comment, Announcement
+from .models import Community, Post, Event, Badge, Profile, Comment, Announcement, Club
 from .forms import PostForm, CommentForm, ProfileForm, EventForm, RegisterForm
 
 
