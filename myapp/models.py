fce464f4d6430310f4ea8932be84e183c368ae2b
mario14guajardo <mario14guajardo@gmail.com>
2025-12-15 06:27:11 -0600

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -13,12 +13,10 @@ class Profile(models.Model):
 
 # ---------------------- ANNOUNCEMENTS ----------------------
 class Announcement(models.Model):
-    title = models.CharField(max_length=200)
-    body = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    text=models.TextField()
 
     def __str__(self):
-        return self.title
+        return self.text[:50]
 
 
 # ---------------------- COMMUNITY ----------------------
