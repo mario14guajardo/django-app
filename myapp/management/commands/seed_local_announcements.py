abc618768c8ea89f8903523a4618d7eaaf70897d
mario14guajardo <mario14guajardo@gmail.com>
2025-12-14 23:35:47 -0600

    Create seed_local_announcements.py

---
+++ myapp/management/commands/seed_local_announcements.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand
+from myapp.models import Announcement
+
+class Command(BaseCommand):
+    help = 'Seed local announcements for testing'
+
+    def handle(self, *args, **kwargs):
+        announcements = [
+            "Welcome to our local server!",
+            "Don't forget to check out the latest updates.",
+            "New features are coming soon!",
+        ]
+
+        for text in announcements:
+            announcement, created = Announcement.objects.get_or_create(
+                text=text
+            )
+            if created:
+                self.stdout.write(self.style.SUCCESS(f"Created announcement: {text}"))
+            else:
+                self.stdout.write(self.style.WARNING(f"Announcement already exists: {text}"))
+
+        self.stdout.write(self.style.SUCCESS("Finished seeding announcements!"))
