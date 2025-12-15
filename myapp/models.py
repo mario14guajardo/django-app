5e58a2e3d6acebee7fd0b0e49383c753e46bac6f
mario14guajardo <mario14guajardo@gmail.com>
2025-12-14 19:58:43 -0600

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -59,3 +59,11 @@ class Badge(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Announcement(models.Model):
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text[:50]
