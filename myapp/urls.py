803fc899d91df0378433fae05a428ad24902eee0
mario14guajardo <mario14guajardo@gmail.com>
2025-12-15 09:45:23 -0600

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -14,6 +14,7 @@ from .views import (
     contact,
     school_map,
     badges,
+    community,
     community_detail,
     create_post_in_community,
     create_post,
@@ -23,7 +24,7 @@ from .views import (
     submit_event_success,
     search,
     feed,
-    events,  # ✅ ADD THIS
+    events,  
 )
 
 urlpatterns = [
@@ -41,6 +42,7 @@ urlpatterns = [
     path('accounts/login/', RedirectView.as_view(url='/login/', permanent=True)),
 
     # Community
+    path('community/', views.community, name='community')
     path('c/<str:name>/', community_detail, name='community_detail'),
     path('c/<str:name>/create/', create_post_in_community, name='create_post_in_community'),
 
